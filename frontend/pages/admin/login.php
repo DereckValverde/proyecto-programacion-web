5588e1c1838d424fe74c0381404c3ccaebde0d3e
Dereck Valverde <derecktiti1234@gmail.com>
2026-07-03 23:00:46 -0600

    Creación del login para administradores

---
+++ frontend/pages/admin/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/admin-login.css">
+    <title>Proyecto</title>
+</head>
+
+<body>
+
+    <body class="login-body">
+
+        <div class="login-container">
+
+            <h1>Acceso Administrativo</h1>
+
+            <form>
+
+                <label for="">Usuario:</label>
+                <input type="text" name="username" id="username" required>
+
+                <label for="">Contraseña</label>
+                <input type="password" name="password" id="password" required>
+
+                <button type="submit">Inciar Sesión</button>
+
+            </form>
+
+        </div>
+    </body>
+
+</html>

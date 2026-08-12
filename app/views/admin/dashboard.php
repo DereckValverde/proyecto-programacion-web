c302e6cdf2b03c94cd29d2ca69b358dc271a0545
Dereck Valverde <derecktiti1234@gmail.com>
2026-08-11 19:14:24 -0600

    fix código css

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@600;700;800&display=swap" rel="stylesheet">
     <link rel="icon" type="image/png" href= <?= BASE_URL . '/public/images/LOGO.png' ?>>
+    <link rel="stylesheet" href="<?= BASE_URL . '/public/css/style-admin.css' ?>">
     <link rel="stylesheet" href=<?=BASE_URL . '/public/css/style-admin-dashboard.css'?>>
 </head>
 
